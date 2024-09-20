ac10dcc86e7f846d94b6fe533318ba47ec26b184
Appitoriadev <contact@appitoria.dev>
2024-09-20 17:44:46 -0500

    Initialize project structure and add initial application files

---
+++ estramipyme/src/main/java/com/estramipyme/repositories/interfaces/IConnection.java
@@ -0,0 +1,9 @@
+package com.estramipyme.repositories.interfaces;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
+
+public interface IConnection {
+    public DataSource dataSource();
+    public JdbcTemplate jdbcTemplate(@Autowired DataSource dataSource);
+}
