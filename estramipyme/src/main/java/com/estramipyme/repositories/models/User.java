ef2f1579106d4c92d35f3c5d991354ef8c34f080
Appitoriadev <contact@appitoria.dev>
2024-09-20 17:26:54 -0500

    Add initial application setup with database models and configuration

---
+++ estramipyme/src/main/java/com/estramipyme/repositories/models/User.java
@@ -0,0 +1,53 @@
+package com.estramipyme.repositories.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "User")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "docId")
+    private String docId;
+
+    @Column(name = "correo")
+    private String correo;
+
+    @Column(name = "contrasena")
+    private String contrasena;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")    
+    private Rol role;
+
+    @Column(name = "fechaNacimiento")
+    @Temporal(TemporalType.DATE)    
+    private Date fechaNacimiento;
+
+    @Column(name = "isPyme")   
+    private Boolean isPyme;
+
+    @Column(name = "isActiveUser")
+    private Boolean isActiveUser;
+}
